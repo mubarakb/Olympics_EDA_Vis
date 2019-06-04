29dc7c150b260e52bb47db85dd68c675ab9995a1
mubarakb <40132755+mubarakb@users.noreply.github.com>
2019-06-03 22:45:04 -0400

    Update dashboard.py

--- olympics_package/dashboard.py
+++ olympics_package/dashboard.py
@@ -16,6 +16,7 @@ colors = {
     'text': '#7FDBFF'
 }
 
+
 app.layout = html.Div(style = {'fontFamily': 'Sans-Serif'}, children=[
     html.H1(
         children = 'Summer Olympics Data From 1980-2016',
