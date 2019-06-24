4d768b60747af587f728c6f300940e332a55560d
mubarakb <40132755+mubarakb@users.noreply.github.com>
2019-06-24 17:44:48 -0400

    Update routes.py

--- olympics_package/routes.py
+++ olympics_package/routes.py
@@ -3,7 +3,6 @@ from flask import render_template, jsonify, json
 # from olympics_package import *
 from olympics_package.dashboard import app
 
-
 @app.server.route('/olympics')
 def olympic_findings():
     return render_template('olympics.html')
